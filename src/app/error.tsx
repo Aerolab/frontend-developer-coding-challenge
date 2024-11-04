2a42ec139a9f95ca4e35fa44e82af9ecfe536349
Nico-Battaglia <nico-battaglia@hotmail.com>
2024-11-04 11:30:39 -0300

    Add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { useEffect } from "react"
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  useEffect(() => {
+    // Optionally log the error to an error reporting service
+    console.error(error)
+  }, [error])
+
+  return (
+    <main className="flex h-full flex-col items-center justify-center">
+      <h2 className="text-center"> Something went wrong! </h2>
+      {error.message && <p className="text-center"> {error.message} </p>}
+      <button
+        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+        onClick={() => reset()}
+      >
+        Try again
+      </button>
+    </main>
+  )
+}
