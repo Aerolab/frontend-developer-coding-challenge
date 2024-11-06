4eb30ad32d206b25cbf82368c664e67b9c3061bb
Nico-Battaglia <nico-battaglia@hotmail.com>
2024-11-06 17:24:57 -0300

    Add logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 import SearchGame from "@/components/SearchGame/SearchGame"
+import Image from "next/image"
+import Tetris from "@/components/animations/Tetris"
 
 type SearchParams = Promise<{ query: string | undefined }>
 
@@ -8,10 +10,21 @@ export default async function Home(props: { searchParams: SearchParams }) {
 
   return (
     <>
-      <main className="flex flex-col gap-8 mt-6">
-        <h1>Gaming Heaven Z</h1>
+      <main className="flex flex-col gap-[20px] mt-12">
+        <div className="flex items-center gap-3">
+          <Image
+            src="/logo.svg"
+            alt="Logo"
+            width={32}
+            height={32}
+            className="border rounded-lg border-pink-600 p-1 shadow"
+          />
+          <h1>Gaming Heaven Z</h1>
+        </div>
 
         <SearchGame query={query} />
+
+        <Tetris />
       </main>
     </>
   )
