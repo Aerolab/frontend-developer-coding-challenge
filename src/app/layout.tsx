248afa5ebd7ba08f1d8a399c8158a6622b0de222
Nico-Battaglia <nico-battaglia@hotmail.com>
2024-11-07 00:06:11 -0300

    Get session to pass to navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import "./globals.css"
 import Navbar from "@/components/Navbar"
 import { Inter } from "next/font/google"
+import { verifySession } from "@/lib/session"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -16,15 +17,17 @@ export const metadata: Metadata = {
   keywords: ["games", "videogames", "gaming", "sega", "nintendo", "play", "pc"],
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
+  const session = await verifySession()
+
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased mx-auto max-w-[1440px]`}>
-        <Navbar />
+        <Navbar session={session} />
         <div className="bg-mobile bgBreakpoint:bg-desktop absolute top-[50px] left-0 right-0 -z-10"></div>
         <div className="grid justify-items-center gap-6">{children}</div>
       </body>
